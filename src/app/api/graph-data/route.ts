7eabdc97132f701dcb74aa74d91ccc7aa3cbd2f4
Armin <rejzovicarmin@gmail.com>
2025-05-19 01:30:23 +0200

    Implement api endpoint which provides random data for 7 days

--- src/app/api/graph-data/route.ts
+++ src/app/api/graph-data/route.ts
@@ -8,5 +8,20 @@ export interface DataPoint {
 export type ApiResponse = DataPoint[];
 
 export async function GET() {
-    return NextResponse.json({message: "Endpoint up and running"});
+    // sleep for 1.2 seconds to simulate delay
+    await new Promise(resolve => setTimeout(resolve, 1200));
+
+    // array from 1 to 7
+    const randomData = Array(7).keys().toArray().map(i => {
+        const timestamp = new Date();
+        timestamp.setDate(timestamp.getDate() - (6 - i));
+
+        const value = Math.floor(Math.random() * 100);
+        return {
+            timestamp: timestamp.toISOString(),
+            value: value,
+        };
+    });
+
+    return NextResponse.json(randomData);
 }
