260447eda7526a90200f7fff7843b523286bad91
Armin <rejzovicarmin@gmail.com>
2025-05-19 02:15:29 +0200

    Implement customer chart completely

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import ProbeChart from "@/components/ProbeChart";
+import CustomerChart from "@/components/CustomerChart";
 
 export default function Home() {
   return (
     <main>
-        <ProbeChart/>
+        <nav className={"h-16 flex items-center p-6 bg-purple-600 font-bold text-white"}>Test Task</nav>
+        <div className={"p-4"}>
+            <CustomerChart/>
+        </div>
     </main>
   );
 }
