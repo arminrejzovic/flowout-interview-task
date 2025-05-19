c8e9b6926abb8c2106babe00e7b1f4615f85d619
Armin <rejzovicarmin@gmail.com>
2025-05-19 02:37:09 +0200

    Add github link to navbar and update README

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import CustomerChart from "@/components/CustomerChart";
+import { FolderGit2 } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-        <nav className={"h-16 flex items-center p-6 bg-purple-600 font-bold text-white"}>Test Task</nav>
+        <nav className={"h-16 flex items-center justify-between p-6 bg-purple-600 font-bold text-white"}>
+            Test Task
+            <Link href="https://github.com/arminrejzovic/flowout-interview-task" target={"_blank"}>
+                <FolderGit2/>
+            </Link>
+        </nav>
         <div className={"p-4"}>
             <CustomerChart/>
         </div>
